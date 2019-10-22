1237ffdd6da9b16facd51d08d1be5d5fe42b4d3a
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-22 20:57:21 +0000

    Add timer2

---
+++ timer2.js
@@ -0,0 +1,17 @@
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+stdin.on('data', (key) => {
+  if (key === 'b') {
+    process.stdout.write('\x07')
+  }
+  if (key >= 1 && key < 10) {
+    console.log(`setting timer for ${key} seconds...`)
+    setTimeout(() => process.stdout.write('\x07'), key * 1000)
+  }
+  if (key === '\u0003') {
+    console.log("Thanks for using me, ciao!")
+    process.exit();
+  }
+});
+console.log('after callback');
